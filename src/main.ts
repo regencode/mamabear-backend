a667b8048a36d2a8be94486ea709308b66900a83
regencode <thomgozalie@gmail.com>
2026-05-03 00:09:54 +0700

    feat: validator and cors configuration

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+      origin: process.env.CORS_ORIGIN,
+      credentials: true,
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+  app.useGlobalPipes(
+      new ValidationPipe({
+          transform: true,
+          whitelist: true,
+          forbidNonWhitelisted: true,
+      }),
+  )
 
   const config = new DocumentBuilder()
     .setTitle('MamaBear API')
