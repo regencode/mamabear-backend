a454290c5b38da54abb5f5c4f882c1bdaf2f8f91
regencode <thomgozalie@gmail.com>
2026-05-02 23:24:45 +0700

    feat: health endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,11 @@ import { PrismaModule } from './prisma/prisma.module';
 import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { HealthcheckModule } from './healthcheck/healthcheck.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [PrismaModule, ProductsModule, UsersModule, AuthModule],
+  imports: [PrismaModule, ProductsModule, UsersModule, AuthModule, HealthcheckModule, HealthModule],
   controllers: [AppController],
   providers: [AppService],
 })
