b699e5909a6d47cad27096c71c721f446f449eea
regencode <thomgozalie@gmail.com>
2026-05-02 23:26:43 +0700

    fix: removed unused module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,10 @@ import { PrismaModule } from './prisma/prisma.module';
 import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
-import { HealthcheckModule } from './healthcheck/healthcheck.module';
 import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [PrismaModule, ProductsModule, UsersModule, AuthModule, HealthcheckModule, HealthModule],
+  imports: [PrismaModule, ProductsModule, UsersModule, AuthModule, HealthModule],
   controllers: [AppController],
   providers: [AppService],
 })
