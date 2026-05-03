29d85797027944688366c0879471bbcea13c86a8
Yous Sibarani <youssibarani17@gmail.com>
2026-05-03 15:00:20 +0700

    feat(auth) implement limiter for auth endpoint using ThrottlerGuard

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,16 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { MailerModule } from '@nestjs-modules/mailer';
 import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
   imports: [
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60_000,
+        limit: 100,
+      },
+    ]),
     ConfigModule.forRoot({
       isGlobal: true,
     }),
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -15,17 +15,31 @@ import { RegisterUserDto } from './dto/register.dto';
 import { RefreshTokenDto } from './dto/refresh-token.dto';
 import { ForgotPasswordDto } from './dto/forgot-password.dto';
 import { ResetPasswordDto } from './dto/reset-password.dto';
+import { Throttle, ThrottlerGuard } from '@nestjs/throttler';
 
+@UseGuards(ThrottlerGuard)
 @ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Throttle({
+    default: {
+      limit: 10,
+      ttl: 300_000,
+    },
+  })
   @Post('/login')
   login(@Body() dto: LoginUserDto) {
     return this.authService.login(dto);
   }
 
+  @Throttle({
+    default: {
+      limit: 5,
+      ttl: 300_000,
+    },
+  })
   @Post('/register')
   register(@Body() dto: RegisterUserDto) {
     return this.authService.register(dto);
@@ -37,21 +51,45 @@ export class AuthController {
     return this.authService.logout(req.user.id);
   }
 
+  @Throttle({
+    default: {
+      limit: 10,
+      ttl: 300_000,
+    },
+  })
   @Post('/refresh')
   refresh(@Body() dto: RefreshTokenDto) {
     return this.authService.refreshToken(dto.refreshToken);
   }
 
+  @Throttle({
+    default: {
+      limit: 5,
+      ttl: 300_000,
+    },
+  })
   @Post('/forgot-password')
   forgotPassword(@Body() dto: ForgotPasswordDto) {
     return this.authService.forgotPassord(dto);
   }
 
+  @Throttle({
+    default: {
+      limit: 5,
+      ttl: 300_000,
+    },
+  })
   @Post('/reset-password/:token')
   resetPassword(@Param('token') token: string, @Body() dto: ResetPasswordDto) {
     return this.authService.resetPassword(token, dto);
   }
 
+  @Throttle({
+    default: {
+      limit: 5,
+      ttl: 300_000,
+    },
+  })
   @Get('/verify-email/:token')
   verifyEmail(@Param('token') token: string) {
     return this.authService.verifyEmail(token);
