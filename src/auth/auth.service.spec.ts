1bb7ec30295aa0a0f9087e5befb9d84c877c9342
Yous Sibarani <youssibarani17@gmail.com>
2026-05-06 20:56:07 +0700

    test(auth) add testcase for refreshToken

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -5,7 +5,7 @@ import { PrismaService } from '@/prisma/prisma.service';
 import { JwtService } from '@nestjs/jwt';
 import { MailService } from './mail.service';
 import * as bcrypt from 'bcrypt';
-import { BadRequestException } from '@nestjs/common';
+import { BadRequestException, UnauthorizedException } from '@nestjs/common';
 import * as crypto from 'crypto';
 
 jest.mock('bcrypt', () => ({
@@ -28,9 +28,12 @@ describe('AuthService', () => {
     findEmail: jest.fn(),
     update: jest.fn(),
     create: jest.fn(),
+    findUserByVerificationToken: jest.fn(),
+    findUserById: jest.fn(),
   };
   const mockJwt = {
     signAsync: jest.fn(),
+    verifyAsync: jest.fn(),
   };
   const mockMail = {
     sendVerificationEmail: jest.fn(),
@@ -49,7 +52,7 @@ describe('AuthService', () => {
     repo = module.get<AuthRepository>(AuthRepository);
     service = module.get<AuthService>(AuthService);
 
-    jest.clearAllMocks();
+    jest.resetAllMocks();
   });
 
   it('should login successfully', async () => {
@@ -195,5 +198,184 @@ describe('AuthService', () => {
     );
 
     expect(mockRepo.create).not.toHaveBeenCalled();
+  });
+
+  it('should verify email successfully', async () => {
+    const user = {
+      id: 'userId-1',
+      verificationToken: 'valid-token',
+      verificationTokenExpiry: new Date('2099-01-01'),
+    };
+
+    const result = {
+      message: 'Email verified successfully',
+    };
+
+    mockRepo.findUserByVerificationToken.mockResolvedValue(user);
+    const resultResPonse = await service.verifyEmail(user.verificationToken);
+    expect(resultResPonse).toEqual(result);
+
+    const responseFindUser = await repo.findUserByVerificationToken(
+      user.verificationToken,
+    );
+
+    expect(mockRepo.findUserByVerificationToken).toHaveBeenCalledWith(
+      user.verificationToken,
+    );
+
+    expect(mockRepo.update).toHaveBeenCalledWith(
+      { id: user.id },
+      expect.objectContaining({
+        isVerified: true,
+        verificationToken: null,
+        verificationTokenExpiry: null,
+      }),
+    );
+  });
+
+  it('should throw BadRequestException for invalid token', async () => {
+    const user = {
+      id: 'userId-1',
+      verificationToken: 'valid-token',
+      verificationTokenExpiry: new Date('2099-01-01'),
+    };
+
+    mockRepo.findUserByVerificationToken.mockResolvedValue(null);
+
+    expect(service.verifyEmail(user.verificationToken)).rejects.toThrow(
+      new BadRequestException('Invalid token'),
+    );
+  });
+
+  it('should throw BadRequestException for token expired', async () => {
+    const user = {
+      id: 'userId-1',
+      verificationToken: 'valid-token',
+      verificationTokenExpiry: new Date('1999-01-01'),
+    };
+
+    mockRepo.findUserByVerificationToken.mockResolvedValue(user);
+
+    expect(service.verifyEmail(user.verificationToken)).rejects.toThrow(
+      new BadRequestException('Token expired'),
+    );
+  });
+
+  it('should refresh token successfully', async () => {
+    const payload = {
+      sub: 'userId-1',
+    };
+    const refreshToken = 'valid-refresh-token';
+
+    const user = {
+      id: 'userId-1',
+      email: 'test@gmail.com',
+      role: 'user',
+      refreshToken: 'hashedRefreshToken',
+      refreshTokenExpiry: new Date('2099-01-01'),
+    };
+    const result = {
+      accessToken: 'newAccessToken',
+    };
+
+    mockJwt.verifyAsync.mockResolvedValue(payload);
+    mockRepo.findUserById.mockResolvedValue(user);
+
+    jest.spyOn(bcrypt, 'compare').mockResolvedValue(true as never);
+    mockJwt.signAsync.mockResolvedValue('newAccessToken');
+
+    const response = await service.refreshToken(refreshToken);
+
+    expect(mockJwt.verifyAsync).toHaveBeenCalledWith(refreshToken, {
+      secret: process.env.JWT_REFRESH_SECRET,
+    });
+    expect(bcrypt.compare).toHaveBeenCalledWith(
+      refreshToken,
+      user.refreshToken,
+    );
+    expect(mockJwt.signAsync).toHaveBeenCalledWith(
+      {
+        sub: user.id,
+        email: user.email,
+        role: user.role,
+      },
+      {
+        secret: process.env.JWT_SECRET,
+        expiresIn: '15m',
+      },
+    );
+    expect(response).toEqual(result);
+  });
+
+  it('should throw new UnauthorizedException for refresh token not found', async () => {
+    const user = {
+      id: 'userId-1',
+      refreshToken: null,
+    };
+    const payload = {
+      sub: 'userId-1',
+    };
+    mockJwt.verifyAsync.mockResolvedValue(payload);
+    mockRepo.findUserById.mockResolvedValue(user);
+
+    await expect(service.refreshToken('token')).rejects.toThrow(
+      new UnauthorizedException('Refresh token not found'),
+    );
+  });
+
+  it('should trhow new UnauthorizedException for Invalid iserId', async () => {
+    const refreshToken = 'valid-refresh-token';
+    const payload = {
+      sub: 'userId-1',
+    };
+
+    mockJwt.verifyAsync.mockResolvedValue(payload);
+    mockRepo.findUserById.mockResolvedValue(null);
+
+    await expect(service.refreshToken(refreshToken)).rejects.toThrow(
+      new UnauthorizedException('User not found'),
+    );
+  });
+
+  it('should throw new UnauthorizedException for invalid refresh token', async () => {
+    const refreshToken = 'valid-refresh-token';
+    const payload = {
+      sub: 'userId-1',
+    };
+    const user = {
+      id: 'userId-1',
+      refreshToken: 'invalid-token',
+    };
+    mockJwt.verifyAsync.mockResolvedValue(payload);
+    mockRepo.findUserById.mockResolvedValue(user);
+
+    jest.spyOn(bcrypt, 'compare').mockResolvedValue(false as never);
+
+    await expect(service.refreshToken(refreshToken)).rejects.toThrow(
+      new UnauthorizedException('Invalid refresh token'),
+    );
+  });
+
+  it('should throw new BadRequestException for refresh token expired', async () => {
+    const payload = {
+      sub: 'userId-1',
+    };
+    const user = {
+      id: 'userId-1',
+      refreshToken: 'valid-token',
+      refreshTokenExpiry: new Date('1999-01-01'),
+    };
+    mockJwt.verifyAsync.mockResolvedValue(payload);
+    mockRepo.findUserById.mockResolvedValue(user);
+    jest.spyOn(bcrypt, 'compare').mockResolvedValue(true as never);
+
+    await expect(service.refreshToken('token')).rejects.toThrow(
+      'Refresh token expired',
+    );
+
+    expect(mockRepo.update).toHaveBeenCalledWith(
+      { id: user.id },
+      { refreshToken: null, refreshTokenExpiry: null },
+    );
   });
 });
