aedd6ebc67c971a78af6479433764ada5a9537ff
regencode <thomgozalie@gmail.com>
2026-05-07 15:31:40 +0700

    fix: type error

--- src/auth/guard/roles.guard.ts
+++ src/auth/guard/roles.guard.ts
@@ -1,4 +1,3 @@
-import { Role } from "@/generated/prisma";
 import { JwtPayload } from "@/types/JwtPayload";
 import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
 import { Reflector } from "@nestjs/core";
@@ -8,7 +7,7 @@ import { Roles } from "../decorators/roles.decorator";
 export class RolesGuard implements CanActivate {
     constructor(private reflector: Reflector) {}
     canActivate(ctx: ExecutionContext): boolean {
-        const allowedRoles: Role[] = this.reflector.get(Roles, ctx.getHandler());
+        const allowedRoles = this.reflector.get(Roles, ctx.getHandler());
         if(!allowedRoles) return true;
         const request: JwtPayload = ctx.switchToHttp().getRequest();
         return (allowedRoles.includes(request.role));
