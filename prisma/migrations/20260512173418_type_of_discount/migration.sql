8fe1e5fddb7c6ea3783258d38538ab608883caae
regencode <thomgozalie@gmail.com>
2026-05-13 00:54:21 +0700

    feat: rename variant folder for merge

--- prisma/migrations/20260512173418_type_of_discount/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `amount` to the `Discount` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Discount" ADD COLUMN     "amount" DECIMAL(12,2) NOT NULL,
-ADD COLUMN     "isPercent" BOOLEAN NOT NULL DEFAULT false;
--- src/variants/variants.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { VariantsService } from './variants.service';
-import { VariantsController } from './variants.controller';
-
-@Module({
-  controllers: [VariantsController],
-  providers: [VariantsService],
-})
-export class VariantsModule {}
