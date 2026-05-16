36c8a0a9f4298977796ffbe777ad42f421742d13
Perhisi Ardhian <perhisi01@gmail.com>
2026-05-16 16:48:19 +0700

    feat(cart): implement CartController with CRUD operations for cart management

---
+++ src/cart/cart.controller.ts
@@ -0,0 +1,71 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Patch,
+  Delete,
+  Body,
+  Req,
+  Param,
+} from '@nestjs/common';
+import { CartService } from './cart.service';
+import { AddToCartDto } from './dto/add-to-cart-dto';
+
+
+@Controller('cart')
+export class CartController {
+  constructor(private readonly cartService: CartService) {}
+
+  // Get Cart
+  @Get()
+  async getCart(@Req() req: any) {
+    const userId = req.user?.id; // from auth (if logged in)
+    const sessionId = req.cookies?.sessionId;
+
+    return this.cartService.getCart(userId, sessionId);
+  }
+  
+ 
+  // Merge Guest Cart → User Cart (for internal use)
+  @Post('merge')
+  async mergeCart(@Req() req: any) {
+    const userId = req.user?.id;
+    const sessionId = req.cookies?.sessionId;
+
+    return this.cartService.mergeCart(userId, sessionId);
+  }
+
+  // Add to Cart
+  @Post('/items')
+  async addToCart(@Body() dto: AddToCartDto, @Req() req: any) {
+    const userId = req.user?.id;
+    const sessionId = req.cookies?.sessionId;
+
+    return this.cartService.addToCart(dto, userId, sessionId);
+  }
+
+  // Update Quantity
+  @Patch('/items/:id')
+  async updateItem(
+    @Param('itemId') itemId: string,
+    @Body('quantity') quantity: number,
+  ) {
+    return this.cartService.updateItemQuantity(itemId, quantity);
+  }
+
+  // Remove Item
+  @Delete('/items/:id')
+  async removeItem(@Param('itemId') itemId: string) {
+    return this.cartService.removeItem(itemId);
+  }
+  
+  // Clear Cart
+  @Delete()
+  async clearCart(@Req() req: any) {
+    const userId = req.user?.id;
+    const sessionId = req.cookies?.sessionId;
+
+    const cart = await this.cartService.getOrCreateCart(userId, sessionId);
+    return this.cartService.clearCart(cart.id);
+  }
+}
--- src/cart/cart.module.ts
+++ src/cart/cart.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CartService } from './cart.service';
 import { PrismaService } from '@/prisma/prisma.service';
+import { CartController } from './cart.controller';
 
 @Module({
   providers: [CartService, PrismaService],
   exports: [CartService],
+  controllers: [CartController],
 })
 export class CartModule {}
