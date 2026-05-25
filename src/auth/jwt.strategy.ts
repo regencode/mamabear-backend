4c554c0c41ea10baa78981c854befe1cacdfac8d
regencode <thomgozalie@gmail.com>
2026-05-25 23:17:53 +0700

    feat(auth): revert from cookie-based auth to bearer auth

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -2,26 +2,24 @@ import 'dotenv/config';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-import { Request } from 'express';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
-      jwtFromRequest: (req: Request) => {
-        return req?.cookies?.['accessToken'] ?? null;
-      },
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: process.env.JWT_ACCESS_SECRET!,
       passReqToCallback: true,
     });
   }
 
-  async validate(req: Request, payload: any) {
+  async validate(payload: any) {
     if (!payload) {
       throw new UnauthorizedException();
     }
     return {
       sub: payload.sub,
+      name: payload.name,
       email: payload.email,
       role: payload.role,
     };
