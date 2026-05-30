6ee6c93f9be87295a1e0617d9afd4bc67c0e60cb
regencode <thomgozalie@gmail.com>
2026-05-30 23:03:39 +0700

    feat: update customer dto

--- src/payment/dto/customer.dto.ts
+++ src/payment/dto/customer.dto.ts
@@ -1,6 +1,19 @@
+import { IsNotEmpty, IsOptional, IsString } from "class-validator"
+
 export class TransactionCustomerDto {
+    @IsString()
+    @IsNotEmpty()
     firstName: string
-    lastName: string
+
+    @IsString()
+    @IsOptional()
+    lastName?: string
+
+    @IsString()
+    @IsNotEmpty()
     email: string
-    phone: string
+
+    @IsString()
+    @IsNotEmpty()
+    phone: string // maybe need to add to jwt token
 }
