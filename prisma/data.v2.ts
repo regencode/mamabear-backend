1ac281f4ec10fb2bd7db7ed4e305699b2756d969
regencode <thomgozalie@gmail.com>
2026-06-04 21:34:04 +0700

    feat: placeholder_hash use password 'admin' for testing

--- prisma/data.v2.ts
+++ prisma/data.v2.ts
@@ -1,8 +1,7 @@
 import * as bcrypt from 'bcrypt';
 import * as crypto from 'crypto';
 
-const PLACEHOLDER_HASH = '$2b$10$placeholderhashplaceholderhashplaceholderha';
-
+const PLACEHOLDER_HASH = '$2b$10$GSB6xe8g7ZwboE4Xo8mMd.4Zvaq6zFuh3UAzfOZK3rRmzINn99/uK' // password is 'admin'
 function slugify(text: string): string {
   return text
     .toLowerCase()
@@ -17,8 +16,7 @@ function genPublicId(): string {
 export const users = [
   {
     email: 'admin@mamabear.id',
-    hashedPassword:
-      '$2b$10$GSB6xe8g7ZwboE4Xo8mMd.4Zvaq6zFuh3UAzfOZK3rRmzINn99/uK',
+    hashedPassword: '$2b$10$GSB6xe8g7ZwboE4Xo8mMd.4Zvaq6zFuh3UAzfOZK3rRmzINn99/uK', // password is 'admin'
     name: 'Admin MamaBear',
     phone: '081200001111',
     role: 'ADMIN' as const,
