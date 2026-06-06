ce394a6cc1dbb7fe1c6d5b5dd4393f6c07b01d3e
regencode <thomgozalie@gmail.com>
2026-06-06 22:09:56 +0700

    feat: should check if the resolved entity by slug is a different instance

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -79,11 +79,11 @@ export class CategoryService {
     if (!resolvedCategory) throw new BadRequestException('Category not found');
 
     if (dto.name) {
-      const generatedSlug = slugify(dto.name, { lower: true, strict: true });
+      const generatedSlug = dto.slug ? dto.slug : slugify(dto.name, { lower: true, strict: true });
       const category = await this.repo.findBySlug(generatedSlug);
-      if (category)
+      if (category && category.id != categoryId) // another category with same resulting slug
         throw new BadRequestException(
-          'Name update generates a slug that already exists',
+          'Name update generates a slug that already exists, or dto.slug is a slug that already exists',
         );
       dto.slug = generatedSlug;
     }
