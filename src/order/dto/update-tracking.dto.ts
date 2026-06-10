c1a9a26ebf5ea676273fa9ef4f7826dce99ddbb8
kebejoan <jmailed.joe@gmail.com>
2026-06-10 23:31:58 +0700

    fix(tracking): change swagger decorator to required

--- src/order/dto/update-tracking.dto.ts
+++ src/order/dto/update-tracking.dto.ts
@@ -1,8 +1,8 @@
-import { ApiPropertyOptional } from '@nestjs/swagger';
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 export class UpdateTrackingDto {
-  @ApiPropertyOptional({
+  @ApiProperty({
     example: 'JNE1234567890',
   })
   @IsString()
