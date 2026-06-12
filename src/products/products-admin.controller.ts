66e96d323b5611a08ea1ab3e8e23865436a2eda9
Yous Sibarani <youssibarani17@gmail.com>
2026-06-12 14:13:32 +0700

    fix(product) add delete endpoint

--- src/products/products-admin.controller.ts
+++ src/products/products-admin.controller.ts
@@ -67,6 +67,10 @@ export class ProductsAdminController {
   update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
     return this.productsService.update(+id, updateProductDto);
   }
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.productsService.remove(+id);
+  }
 
   @Post(':id/variants')
   createProductVariant(
